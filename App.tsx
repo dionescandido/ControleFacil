648b24a707d74f723a5a7bacb821a1d325a4303d
Dΐση૯ร ® <dionescandido181@hotmail.com>
2021-11-09 08:19:16 -0300

    Inserido estrutura React

--- App.tsx
+++ App.tsx
@@ -1,27 +1,11 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import { Home } from './src/view/home';
+import { PerfilUsuario } from './src/view/usuario/perfil';
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to notifications"
-      />
-    </View>
-  );
-}
 
-function NotificationsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
-  );
-}
 
 const Drawer = createDrawerNavigator();
 
@@ -29,8 +13,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Perfil" component={PerfilUsuario} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
